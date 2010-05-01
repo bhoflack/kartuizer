628b81239cc5a8c078e95ac3a3433a90126b1ae7
Brecht Hoflack <brecht@iMac-van-iMac.local>
2010-05-01 11:48:31 +0200

    Added markdown as wiki parser.

--- app/middleware/wiki.py
+++ app/middleware/wiki.py
@@ -5,33 +5,29 @@ wikimiddleware.py
 Translate wikipage to htmlpage.
 """
 
+from markdown import markdown
+
 class WikiMiddleware:
 	
-	def __init__(self, app, args):
-		self.app = app
-	
-	def __call__(self, environ, start_response):
+	def process_response(self, request, response):
 		"""Catch the wiki response and translate to html.
 		
-		   >>> response = MockHttpResponse("This is my wiki response [[url|text]]")
-		   >>> wm = WikiMiddleware()
-		   >>> wm.process_response(None, response)
-			 >>> response.content
-			 'This is my wiki response <a href="/url">text</a>'
+		>>> response = MockHttpResponse("This is my wiki response [text](url)")
+		>>> wm = WikiMiddleware()
+		>>> wm.process_response(None, response)
+		http response
+		>>> response.content
+		u'<p>This is my wiki response <a href="url">text</a></p>'
 		"""
-		buffer = ""
-		app_iter = self.app(environ, start_response)
-		for line in app_iter:
-			buffer.append(line)
-		return [self.replace_wiki_words(buffer)] 
-			 
-			
-	def replace_wiki_words(self, content):
-		return content.replace("[[url|text]]", "<a href=\"/url\">text</a>")
+		response.content = markdown(response.content) 
+		return response
 
 class MockHttpResponse:
 	def __init__(self, response):
 		self.content = response
+		
+	def __repr__(self):
+		return "http response"
 
 
 if __name__ == "__main__":
